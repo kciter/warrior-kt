4d535538b034e2656e671357c0e669bc687f66e5
Lee Sun-Hyoup <kciter@naver.com>
2024-02-04 23:27:19 +0900

    Add dependencies for kotlin scripting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+val kotlinVersion: String by extra("1.9.22")
+
 plugins {
-  kotlin("jvm") version "1.9.0"
+  kotlin("jvm") version "1.9.22"
 }
 
 allprojects {
--- core/build.gradle.kt
+++ core/build.gradle.kt
@@ -1,5 +1,12 @@
 val artifactId = "warrior-kt-core"
+val kotlinVersion: String by rootProject.extra
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+  implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+
+  implementation("org.jetbrains.kotlin:kotlin-scripting-common:$kotlinVersion")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:$kotlinVersion")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:$kotlinVersion")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:$kotlinVersion")
 }
