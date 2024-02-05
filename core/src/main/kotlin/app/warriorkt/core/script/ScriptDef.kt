03674079ad15774eb07f70963f7b45c9583b2e35
Lee Sun-Hyoup <kciter@naver.com>
2024-02-05 10:31:04 +0900

    Import packages

--- core/src/main/kotlin/app/warriorkt/core/script/ScriptDef.kt
+++ core/src/main/kotlin/app/warriorkt/core/script/ScriptDef.kt
@@ -1,5 +1,33 @@
 package app.warriorkt.core.script
 
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import kotlin.script.experimental.annotations.KotlinScript
+import kotlin.script.experimental.api.EvaluationResult
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptCollectedData
+import kotlin.script.experimental.api.ScriptCompilationConfiguration
+import kotlin.script.experimental.api.ScriptConfigurationRefinementContext
+import kotlin.script.experimental.api.asSuccess
+import kotlin.script.experimental.api.collectedAnnotations
+import kotlin.script.experimental.api.defaultImports
+import kotlin.script.experimental.api.dependencies
+import kotlin.script.experimental.api.onSuccess
+import kotlin.script.experimental.api.refineConfiguration
+import kotlin.script.experimental.api.with
+import kotlin.script.experimental.dependencies.CompoundDependenciesResolver
+import kotlin.script.experimental.dependencies.DependsOn
+import kotlin.script.experimental.dependencies.FileSystemDependenciesResolver
+import kotlin.script.experimental.dependencies.Repository
+import kotlin.script.experimental.dependencies.maven.MavenDependenciesResolver
+import kotlin.script.experimental.dependencies.resolveFromScriptSourceAnnotations
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.JvmDependency
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
+import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
+import kotlin.script.experimental.jvmhost.createJvmCompilationConfigurationFromTemplate
+
 @KotlinScript(
   fileExtension = ".kts",
   compilationConfiguration = ScriptWithMavenDepsConfiguration::class
